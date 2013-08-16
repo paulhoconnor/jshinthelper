82fde6a67ad701be05d18d3d3ca9f25d53f2538a
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-08-16 14:30:43 +0000

    Resolve jshintrc path as relative.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var path = require('path');
 
-process.argv.push('--exclude', 'node_modules', '--config', path.resolve('node_modules', path.basename(__dirname), 'jshintrc'));
+process.argv.push('--exclude', 'node_modules', '--config', path.resolve('.', 'node_modules', path.basename(__dirname), 'jshintrc'));
 
 if (process.argv.indexOf('.') === -1) {
     // push directory if jshint isn't run with '.'
     process.argv.push(path.resolve(__dirname, '..', '..'));
 }
+
+console.log(process.argv);
 
 require('jshint/bin/jshint');
