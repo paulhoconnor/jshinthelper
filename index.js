a663ef84768393b85c127a11a9ec08f91135fdb0
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-08-16 13:58:13 +0000

    add directory to lint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path = require('path');
 
 process.argv.push('--exclude', 'node_modules', '--config', path.resolve('node_modules', path.basename(__dirname), 'jshintrc'));
-console.log(process.argv);
-console.log(require('jshint/bin/jshint'));
+process.argv.push(path.resolve(__dirname, '..', '..'));
+require('jshint/bin/jshint');
